ccbf722a3d6921d6694a9ee46ef34a353c322cc0
Hanifan Mutashim Billah <hanifanmutasim@gmail.com>
2025-12-15 14:31:11 +0700

    https bagst

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const isProduction = import.meta.env.MODE === 'production';
 
 export const API_URL = isProduction 
-  ? 'https://castellatus-zander-puristically.ngrok-free.dev' // <-- Paste link Ngrok baru di sini
+  ? 'castellatus-zander-puristically.ngrok-free.dev' // <-- Paste link Ngrok baru di sini
   : 'http://localhost:3000';
