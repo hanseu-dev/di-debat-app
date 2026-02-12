b0d6050d28ba9ea85e7d6bd83c8272250289f261
Hanifan Mutashim Billah <hanifanmutasim@gmail.com>
2026-02-12 11:58:23 +0700

    add API_URL ke edgeone server

--- client/src/config.js
+++ client/src/config.js
@@ -2,4 +2,5 @@ const isProduction = import.meta.env.MODE === 'production';
 
 export const API_URL = isProduction 
   ? 'https://castellatus-zander-puristically.ngrok-free.dev' // <-- Paste link Ngrok baru di sini
-  : 'http://localhost:3000';
+  // : 'http://localhost:3000'
+  : 'di-debat-server.edgeone.dev';
